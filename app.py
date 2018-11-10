37c5d964015d278f3e8546449c3dfaef76fb309f
Ryusei Suzuki <s15526@salesio-sp.ac.jp>
2018-11-10 20:34:02 +0900

    update functions

--- app.py
+++ app.py
@@ -6,24 +6,34 @@ import pyodbc
 
 app = Flask(__name__)
 
-@app.route('/wifi', methods=['POST'])
-def method_name():
+@app.route('/wifi/addPoints', methods=['POST'])
+def add_points():
     if request.method == 'POST':
-        pass
+        req = request.get_json()
    pass
 
-@app.route('/wifi', methods=['POST'])
-def method_name():
-    if request.method == 'POST':
-        pass
+@app.route('/wifi/getPoints', methods=['GET'])
+def get_points():
+    if request.method == 'GET':
+        latitude = request.args.get('latitude')
+        longitude = request.args.get('longitude')
+        distance = request.args.get('distance')
    pass
-   
-@app.route('/wifi', methods=['POST'])
-def method_name():
+
+@app.route('/wifi/updatePoint', methods=['POST'])
+def update_points():
     if request.method == 'POST':
-        pass
+        req = request.get_json()
    pass
+
+def sql_add_query():
+    pass
+
+def sql_get_query():
+    pass
+
+def sql_update_query():
+    pass
 
 if __name__ == '__main__':
-    app.run()
-    
+    app.run(debug=True)
