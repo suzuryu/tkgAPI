fbe249ab3df555eaf675e8468c028ae33d3c9450
Ryusei Suzuki <s15526@salesio-sp.ac.jp>
2018-11-22 11:28:20 +0900

    func

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 def add_points():
     if request.method == 'POST':
         req = request.get_json()
-   pass
+        for r in req:
+            sql_add_query(r)
+
+        return {'200':'success!'}
 
 @app.route('/wifi/getPoints', methods=['GET'])
 def get_points():
@@ -18,22 +21,27 @@ def get_points():
         latitude = request.args.get('latitude')
         longitude = request.args.get('longitude')
         distance = request.args.get('distance')
-   pass
+
+        return {sql_get_query(latitude, longitude, distance), {'200':'success!'}}
+
 
 @app.route('/wifi/updatePoints', methods=['POST'])
 def update_points():
     if request.method == 'POST':
-        req = request.get_json()
-   pass
+        req = request.get_json{'200':'success!'}()
+        for r in req:
+            sql_update_query(r)
 
-def sql_add_query():
-    pass
+        return {'200':'success!'}
 
-def sql_get_query():
-    pass
+def sql_add_query(point):
+    sql_query = ""
 
-def sql_update_query():
-    pass
+def sql_get_query(latitude, longitude, distance):
+    sql_query = ""
+
+def sql_update_query(point):
+    sql_query = ""
 
 if __name__ == '__main__':
     app.run(debug=True)
