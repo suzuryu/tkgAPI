6b2e5bb370bb3d90651d339c1f6b9e951b3b6ee3
Ryusei Suzuki <s15526@salesio-sp.ac.jp>
2018-12-06 21:22:28 +0900

    fix

--- app.py
+++ app.py
@@ -61,28 +61,28 @@ def sql_add_query(point):
 
 
 def sql_get_by_distance_query(latitude, longitude, distance):
-    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME
-                + "WHERE MBRIntersects(GeomFromText('LineString({x0} {y0}, {x1} {y2})'), geo)".format(
-                        longitude - distance, latitude - distance, longitude + distance, latitude + distance)
+    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME \
+                + "WHERE MBRIntersects(GeomFromText('LineString({x0} {y0}, {x1} {y2})'), geo)".format( \
+                    longitude - distance, latitude - distance, longitude + distance, latitude + distance)
 
     return execute_sql(sql_query)
 
 def sql_get_by_name_query(name):
-    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME
+    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME \
                 + "WHERE name LIKE '{0}%'".format(name)
 
     return execute_sql(sql_query)
 
 def sql_get_by_id_query(id):
-    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME
+    sql_query = "SELECT id, name, ssid, address, postCode, hpUrl, X(geoPoint), Y(geoPoint) FROM " + TABLE_NAME \
                     + "WHERE id == {0}".format(id)
 
     return execute_sql(sql_query)
 
 def sql_update_query(point):
-    sql_query = "UPDATE " + TABLE_NAME
-                 + " SET name = ?, ssid = ?, address = ?, postCode = ?, hpUrl = ?, geoPoint = GeomFormText('POINT(? ?)')"
-                 + " WHERE id == ?"
+    sql_query = "UPDATE " + TABLE_NAME \
+                + " SET name = ?, ssid = ?, address = ?, postCode = ?, hpUrl = ?, geoPoint = GeomFormText('POINT(? ?)')" \
+                + " WHERE id == ?"
     values = (point['name'], point['ssid'], point['address'], point['postCode'],  \
                     point['hpUrl'], point['longitude'], point['latitude'], point['id'])
 
