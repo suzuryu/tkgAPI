5b6c0a12e3dbab272074a628ed66a4d0f6f1f3c1
ammYou <ammyou.nf@gmail.com>
2019-01-10 11:55:36 +0900

    fix makeDB

--- makeDB.py
+++ makeDB.py
@@ -8,16 +8,15 @@ def create_table():
     with closing(sqlite3.connect(DB_NAME)) as conn:
         conn.enable_load_extension(True)
         c = conn.cursor()
-        home_dir = os.environ['HOME'] 
         # load spatiaLite
-        c.execute("SELECT load_extension('{home}/prefix/lib/mod_spatialite.so');".format(home=home_dir))
+        c.execute("SELECT load_extension('var/www/tkgAPI/prefix/lib/mod_spatialite.so');")
 
         print("load spatiaLite")
         # init spatiaLite
         c.execute('''SELECT InitSpatialMetaData();''')
         print("init")
         create_table = '''CREATE TABLE IF NOT EXISTS wifi (id INTEGER PRIMARY KEY AUTOINCREMENT, name CHAR(100), ssid CHAR(50) NOT NULL, address CHAR(100), postCode INT, hpUrl CHAR(100), geoPoint GEOMETORY NOT NULL);'''
-        
+
         c.execute(create_table)
         print("create table")
 
