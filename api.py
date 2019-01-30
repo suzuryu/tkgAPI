8b89fedfa806b201fcaf317f9d3217c23b97542c
ammYou <ammyou.nf@gmail.com>
2019-01-30 18:32:26 +0900

    update db var

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import pyodbc
 from config.run_config import APP_DEBUG, APP_TESTING
 
 TABLE_NAME = 'wifi'
-DB_NAME = 'wifiInformations'
+DB_NAME = '/home/you/git/tkgAPI/wifiInformations'
 
 
 def create_app(debug=APP_DEBUG, testing=APP_TESTING, config_overrides=None):
