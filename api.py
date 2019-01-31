183f09c87d07c2efa60947005c0867b05dd63463
ammYou <ammyou.nf@gmail.com>
2019-01-31 17:07:08 +0900

    fix load ex

--- api.py
+++ api.py
@@ -189,6 +189,9 @@ def create_app(debug=APP_DEBUG, testing=APP_TESTING, config_overrides=None):
     def execute_sql(sql_query, values=()):
         con = pyodbc.connect(r'DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE='+ DB_NAME + ';Trusted_connection=yes')
         cur = con.cursor()
+        cur.execute("SELECT load_extension('mod_spatialite');")
+        cur.execute("SELECT load_extension('libspatialite');")
+
         try:
             if len(values) != 0:
                 cur.execute(sql_query,values)
